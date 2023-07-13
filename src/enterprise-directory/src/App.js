1b1cd88f4ee3e616af4102ff4a1deed0c9625198
dennypuka12 <denny.puka@uconn.edu>
2023-07-13 16:26:38 -0400

    should fix conflict

--- src/enterprise-directory/src/App.js
+++ src/enterprise-directory/src/App.js
@@ -8,12 +8,8 @@ import "./App.css"
 function App() {
 
   const [data, setData] = useState([]);
-<<<<<<< HEAD
   const [searchResults, setSearchResults] = useState([])
   const [seen, setSeen] = useState(false)
-=======
-  const [searchResults, setSearchResults] = useState(null)
->>>>>>> 8134dda4b14c8c6bda347001978e47c93ab90a5a
 
   useEffect(()=>{
     const fetchData = async()=>{
