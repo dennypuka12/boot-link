994f2d6829d2f7bafa98954df090bcdb83e8085e
Denny <dpuka@travelers.com>
2023-07-13 11:08:55 -0400

    Finishing adding some extra Express routes

--- src/server/mongodao.js
+++ src/server/mongodao.js
@@ -34,3 +34,43 @@ module.exports.findEmployee = async function (name, callback) {
     });
   };
   
+module.exports.findEmpoyeesByLocation = async function (location, callback) {
+    var col = dbPool.collection('fakeEmployees');
+    col.find({  workLocation: { $regex: new RegExp(location, 'i') } }).toArray(async(err, employees) => {
+    if (!err) {
+            console.log({employees})
+            callback(null, employees[0]);
+          } else {
+            callback("Failed to find employee", undefined);
+          }    
+    });
+  };
+
+  // retrieve single employee
+module.exports.findEmployeeByPhone = async function (phoneNumber, callback) {
+
+    let cleanedPhoneNumber = phoneNumber.replace(/\D/g, '');
+    var col = dbPool.collection("fakeEmployees");
+    col.find({ 'phoneNumber': { $regex: new RegExp(cleanedPhoneNumber, 'i')} }).toArray(async(err, employees) => {
+      if (!err) {
+        console.log({employees})
+        callback(null, employees[0]);
+      } else {
+        callback("Failed to find employee", undefined);
+      }
+    });
+  };
+
+    // retrieve salary info
+module.exports.findEmployeeBySalary = async function (salary, callback) {
+    let fixedSalary = '$' + salary;
+    var col = dbPool.collection("fakeEmployees");
+    col.find({ 'salary': { $gt: fixedSalary } }).toArray(async(err, employees) => {
+      if (!err) {
+        console.log({employees})
+        callback(null, employees[0]);
+      } else {
+        callback("Failed to find employee", undefined);
+      }
+    });
+  };
